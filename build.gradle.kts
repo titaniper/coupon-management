2850c0d37c3efd6ede16cb5fd1b1145f4a96866b
titaniper <flutter.kr@gmail.com>
2024-07-20 21:57:02 +0900

    chore: Redis, Mysql 구성

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,8 +36,8 @@ subprojects {
     }
 
     dependencies {
-//        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-//        implementation("org.springframework.boot:spring-boot-starter-data-redis")
+        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+        implementation("org.springframework.boot:spring-boot-starter-data-redis")
         compileOnly("org.projectlombok:lombok")
         annotationProcessor("org.projectlombok:lombok")
         runtimeOnly("com.h2database:h2")
