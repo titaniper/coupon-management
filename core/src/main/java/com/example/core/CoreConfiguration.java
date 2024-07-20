3bcef3e8a1853e179bb43ac930fa9e56c7a4737f
titaniper <flutter.kr@gmail.com>
2024-07-20 21:38:13 +0900

    chore: 멀티 모듈 구성

---
+++ core/src/main/java/com/example/core/CoreConfiguration.java
@@ -0,0 +1,9 @@
+package com.example.core;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
+public class CoreConfiguration {
+}
